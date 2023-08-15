83d35e136eea88f6c1746f024ed9ae97eecb7c04
Simon de la Court <simondelacourt@users.noreply.github.com>
2023-08-15 17:05:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "NFCPassportReader",
-            dependencies: ["OpenSSL"]),
+        .target(name: "NFCPassportReader"),
     ]
 )
 
